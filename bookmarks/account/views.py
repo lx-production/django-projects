146d0705d3459f52218e2fc43465ac5ec1c22225
Huy Nguyen <willber.es@gmail.com>
2017-12-06 21:57:00 -0800

    Login Logout

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
+# from django.contrib import messages
 from .forms import LoginForm
 
 
@@ -25,3 +25,7 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
+
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
